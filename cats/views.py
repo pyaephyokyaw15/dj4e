1a72cd07f8a3a1a024973f4f76f26e2dd09509fd
pyaephyokyaw15 <pyaekyaw1571999@gmail.com>
2022-04-27 14:55:44 +0630

    Cat App is functionally complete.

--- cats/views.py
+++ cats/views.py
@@ -25,3 +25,51 @@ class BreedView(LoginRequiredMixin, View):
         return render(request, 'cats/breed_list.html', ctx)
 
 
+
+
+# Take the easy way out on the main table
+# These views do not need a form because CreateView, etc.
+# Build a form object dynamically based on the fields
+# value in the constructor attributes
+class CatCreate(LoginRequiredMixin, CreateView):
+    model = Cat
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
+
+
+class CatUpdate(LoginRequiredMixin, UpdateView):
+    model = Cat
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
+
+
+class CatDelete(LoginRequiredMixin, DeleteView):
+    model = Cat
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
+
+
+
+class BreedCreate(LoginRequiredMixin, CreateView):
+    model = Breed
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
+
+
+class BreedUpdate(LoginRequiredMixin, UpdateView):
+    model = Breed
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
+
+
+class BreedDelete(LoginRequiredMixin, DeleteView):
+    model = Breed
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
+
+# We use reverse_lazy rather than reverse in the class attributes
+# because views.py is loaded by urls.py and in urls.py as_view() causes
+# the constructor for the view class to run before urls.py has been
+# completely loaded and urlpatterns has been processed.
+
+# References
